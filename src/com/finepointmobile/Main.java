c3e78c0c35368c2e4a45bdd53e7769484940cf59
Daniel Malone <danieljmalone@gmail.com>
2017-07-24 22:54:25 -0500

    get user input for folder name

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -1,18 +1,23 @@
 package com.finepointmobile;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hi!");
 
-        File file = new File("/Users/danielmalone/Documents/_JavaDir");
+        Scanner reader = new Scanner(System.in);
+        String folderName = reader.nextLine();
+        System.out.println(folderName);
+
+        File file = new File("/Users/danielmalone/Documents/" + folderName);
         if (!file.exists()) {
             System.out.println("Creating folder...");
             file.mkdir();
         } else {
-            System.out.println("Already exists.");
+            System.out.println(folderName + " already exists.");
         }
     }
 }
