e15b790101df4a953ece730814345177ece81ec7
Daniel Malone <danieljmalone@gmail.com>
2017-07-24 21:57:50 -0500

    create new directory

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -1,8 +1,18 @@
 package com.finepointmobile;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hi!");
+
+        File file = new File("/Users/danielmalone/Documents/_JavaDir");
+        if (!file.exists()) {
+            System.out.println("Creating folder...");
+            file.mkdir();
+        } else {
+            System.out.println("Already exists.");
+        }
     }
 }
